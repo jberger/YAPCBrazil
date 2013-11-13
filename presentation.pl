a7da819ee8261c565a21b952af489c8a2455b454
Joel Berger <joel.a.berger@gmail.com>
2013-11-13 12:11:18 -0200

    switch to mathjax cdn

--- presentation.pl
+++ presentation.pl
@@ -14,8 +14,8 @@ my $pres = {
   templates => File::Spec->catdir($dir, 'slides'),
   extra_js  => [
     'myjs.js', 
-    '/MathJax/MathJax.js?config=default',
-    # 'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=default',
+    # '/MathJax/MathJax.js?config=default',
+    'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=default',
   ],
   extra_css => 'mycss.css',
   finally => sub {
